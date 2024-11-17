99a035f2fd6f8ba099ee2276f89d4a30f3fe9104
Nacir <nacir16mahfoudhi@gmail.com>
2024-11-17 12:21:41 +0100

    t

---
+++ MachineDashboarding/Interfaces/IMachineManagement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineDashboardingDomain.Interfaces
+{
+    public interface IMachineManagement
+    {
+
+        public void startTracing(int MachineId);
+
+    }
+}
